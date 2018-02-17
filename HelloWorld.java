f034ec54f3b413798443b8d715ecc80e603681e8
Fabien POIRIER <poirier-f@informiasa.local>
2018-02-17 22:05:10 +0100

    //

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,15 @@
+package oata;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class HelloWorld {
+	private static final Logger logger = LogManager.getLogger("HelloWorld");
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+		logger.info("Hello World");          // the old SysO-statement
+		// Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
-
 }
---
+++ HelloWorldTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+    
+}
